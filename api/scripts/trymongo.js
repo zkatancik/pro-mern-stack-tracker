de5444ddf2dbb8635cfda2eca9ed4bcea5f813cc
zkatancik <katancik.z@husky.neu.edu>
2020-06-16 23:05:26 -0700

    pushing section 2, added .env functionality

--- api/scripts/trymongo.js
+++ api/scripts/trymongo.js
@@ -1,12 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost/issuetracker';
-
-// Atlas URL - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
-
-// mLab URL - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
+const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 
 function testWithCallbacks(callback) {
     console.log('\n--- testWithCallbacks ---');
@@ -16,7 +10,7 @@ function testWithCallbacks(callback) {
             callback(err);
             return;
         }
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB URL', url);
 
         const db = client.db();
         const collection = db.collection('employees');
@@ -48,7 +42,7 @@ async function testWithAsync() {
     const client = new MongoClient(url, { useNewUrlParser: true });
     try {
         await client.connect();
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB URL', url);
         const db = client.db();
         const collection = db.collection('employees');
     
