9389f67e1b5231666cc61c9139367f975b0af70e
zkatancik <katancik.z@husky.neu.edu>
2020-06-23 20:57:46 -0700

    linted da code

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: { app: ['./src/App.jsx'] },
-    output: {
+  output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
     publicPath: '/',
