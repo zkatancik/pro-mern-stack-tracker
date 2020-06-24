5fb4000838038076fd9b66b7de4c427743a47d46
zkatancik <katancik.z@husky.neu.edu>
2020-06-23 20:51:59 -0700

    added browser routing vs hash routing as before

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
@@ -31,9 +32,15 @@ if (apiProxyTarget) {
 
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
   || 'http://localhost:3000/graphql';
+
 const env = { UI_API_ENDPOINT };
+
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
 });
 
 const port = process.env.UI_SERVER_PORT || 8000;
