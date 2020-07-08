83c3850cee3ace2e130fcccdc31216aba203ec8b
zkatancik <katancik.z@husky.neu.edu>
2020-07-08 15:21:43 -0700

    added ui changes to pagination feature

--- ui/src/Toast.jsx
+++ ui/src/Toast.jsx
@@ -20,7 +20,7 @@ export default class Toast extends React.Component {
     } = this.props;
     return (
       <Collapse in={showing}>
-        <div style={{ position: 'fixed', bottom: 20, left: 20 }}>
+        <div style={{ position: 'fixed', bottom: 20, left: 20, zIndex: 10, }}>
           <Alert bsStyle={bsStyle} onDismiss={onDismiss}>
             {children}
           </Alert>
