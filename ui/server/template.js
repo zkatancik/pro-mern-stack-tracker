6b684fa9a3b52e91e627b03027b42a860d5c1fba
zkatancik <katancik.z@husky.neu.edu>
2020-07-13 22:52:04 -0700

    added google sign in

--- ui/server/template.js
+++ ui/server/template.js
@@ -9,6 +9,9 @@ export default function template(body, data) {
   <title>Pro MERN Stack</title>
   <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <script src="https://apis.google.com/js/api:client.js"></script>
+  
   <style>
     table.table-hover tr {cursor: pointer;}
     .panel-title a {display: block; width: 100%; cursor: pointer;}
