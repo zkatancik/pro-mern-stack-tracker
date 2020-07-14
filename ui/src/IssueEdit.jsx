3078cb3ece9aff306dbeaa3bb366f1bd003bdeb8
zkatancik <katancik.z@husky.neu.edu>
2020-07-14 16:19:45 -0700

    disabled submit in edit, trash, and close buttons when not signed in

--- ui/src/IssueEdit.jsx
+++ ui/src/IssueEdit.jsx
@@ -12,6 +12,7 @@ import DateInput from './DateInput.jsx';
 import TextInput from './TextInput.jsx';
 import withToast from './withToast.jsx';
 import store from './store.js';
+import UserContext from './UserContext.js';
 
 class IssueEdit extends React.Component {
   static async fetchData(match, search, showError) {
@@ -144,6 +145,7 @@ class IssueEdit extends React.Component {
     const { issue: { title, status } } = this.state;
     const { issue: { owner, effort, description } } = this.state;
     const { issue: { created, due } } = this.state;
+    const user = this.context;
 
     return (
       <Panel>
@@ -247,7 +249,13 @@ class IssueEdit extends React.Component {
             <FormGroup>
               <Col smOffset={3} sm={6}>
                 <ButtonToolbar>
-                  <Button bsStyle="primary" type="submit">Submit</Button>
+                  <Button
+                    disabled={!user.signedIn}
+                    bsStyle="primary"
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
                   <LinkContainer to="/issues">
                     <Button bsStyle="link">Back</Button>
                   </LinkContainer>
@@ -268,6 +276,8 @@ class IssueEdit extends React.Component {
     );
   }
 }
+
+IssueEdit.contextType = UserContext;
 
 const IssueEditWithToast = withToast(IssueEdit);
 IssueEditWithToast.fetchData = IssueEdit.fetchData;
